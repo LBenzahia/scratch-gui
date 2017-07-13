c03a6e929fd14b7228158005ad04caaf5af84053
Paul Kaplan <pkaplan@media.mit.edu>
2017-07-13 15:53:01 -0400

    Add dynamics compressor for echo

--- src/lib/audio/effects/echo-effect.js
+++ src/lib/audio/effects/echo-effect.js
@@ -9,15 +9,24 @@ class EchoEffect {
         this.delay.delayTime.value = delayTime;
         this.decay = this.audioContext.createGain(); // @todo chain
         this.decay.gain.value = 0.5;
+
+        this.compressor = this.audioContext.createDynamicsCompressor();
+        this.compressor.threshold.value = -50;
+        this.compressor.knee.value = 40;
+        this.compressor.ratio.value = 12;
+        this.compressor.attack.value = 0;
+        this.compressor.release.value = 0.25;
+
         this.output = this.audioContext.createGain();
 
         this.input.connect(this.delay);
         if (delayTime !== 0) {
-            this.delay.connect(this.output);
+            this.delay.connect(this.compressor);
         }
-        this.input.connect(this.output);
+        this.input.connect(this.compressor);
         this.delay.connect(this.decay);
         this.decay.connect(this.delay);
+        this.compressor.connect(this.output);
     }
 
     dispose () {
