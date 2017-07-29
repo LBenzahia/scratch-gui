150f5aec142bb55ed07eb83dedc3d74514993671
LBenzahia <lakhdar.benzahia@gmail.com>
2017-07-29 19:53:35 +0100

    adds arabic to locale

--- src/locale.js
+++ src/locale.js
@@ -1,3 +1,4 @@
+import localeDataAr from 'react-intl/locale-data/ar';
 import localeDataEn from 'react-intl/locale-data/en';
 import localeDataEs from 'react-intl/locale-data/es';
 import localeDataFr from 'react-intl/locale-data/fr';
@@ -5,6 +6,11 @@ import localeDataFr from 'react-intl/locale-data/fr';
 import messages from '../locale/messages.json'; // eslint-disable-line import/no-unresolved
 
 export default {
+    ar: {
+      name: 'الْعَرَبِيَّة',
+      localeData: localeDataAr,
+      messages: messages.ar
+    },
     en: {
         name: 'English',
         localeData: localeDataEn,
